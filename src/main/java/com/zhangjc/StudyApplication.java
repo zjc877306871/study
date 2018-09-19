f6e7f9ac616e8ff952b64cd69482a544737d3a44
zhangjc <1204880901@qq.com>
2018-09-19 15:34:06 +0800

    java基础多线程

--- src/main/java/com/zhangjc/StudyApplication.java
+++ src/main/java/com/zhangjc/StudyApplication.java
@@ -3,6 +3,9 @@ package com.zhangjc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动入口
+ */
 @SpringBootApplication
 public class StudyApplication {
 
