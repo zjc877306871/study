6169e8038e45d3152bcfc5159414087e6b0b2f86
zhangjc <1204880901@qq.com>
2018-09-20 11:25:51 +0800

    优化公共线程测试方法

--- src/main/java/com/zhangjc/javaBasic/queue/blockingQueue/ProductThread.java
+++ src/main/java/com/zhangjc/javaBasic/queue/blockingQueue/ProductThread.java
@@ -4,6 +4,8 @@ import com.zhangjc.javaBasic.queue.entry.People;
 
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * 模拟生产者生成信息插入队列
@@ -11,7 +13,24 @@ import java.util.concurrent.BlockingQueue;
  */
 public class ProductThread implements Runnable{
 
+    public static final String ADD = "add"; //队列满时，抛出异常
+    public static final String OFFER = "offer";//返回添加结果  true/false
+    public static final String PUT = "put"; //如果队列满。线程一直阻塞
+
     BlockingQueue queue ;
+    private String monthName;
+    private String firstOrLast;
+
+    public ProductThread(BlockingQueue queue, String monthName, String firstOrLast) {
+        this.queue = queue;
+        this.monthName = monthName;
+        this.firstOrLast = firstOrLast;
+    }
+
+    public ProductThread(BlockingQueue queue, String monthName) {
+        this.queue = queue;
+        this.monthName = monthName;
+    }
 
     public ProductThread(BlockingQueue queue) {
         this.queue = queue;
@@ -19,9 +38,92 @@ public class ProductThread implements Runnable{
 
     @Override
     public void run() {
-        Random r = new Random();
-        People pp = new People();
-        pp.setAge(2 + r.nextInt());
-        queue.add(pp);
+        if(monthName == null || monthName == ""){
+            System.out.println("无法识别是哪一个方法操作");
+            throw new RuntimeException("null指针");
+        }else if(ADD == monthName){
+            if(queue instanceof LinkedBlockingDeque){
+                if("1".equals(firstOrLast)){
+                    Random r = new Random();
+                    People pp = new People();
+                    pp.setAge(2 + r.nextInt());
+                    LinkedBlockingDeque queueNew = (LinkedBlockingDeque) queue;
+                    queueNew.addFirst(pp);
+                }else{
+                    Random r = new Random();
+                    People pp = new People();
+                    pp.setAge(2 + r.nextInt());
+                    LinkedBlockingDeque queueNew = (LinkedBlockingDeque) queue;
+                    queueNew.addLast(pp);
+                }
+            }else{
+                Random r = new Random();
+                People pp = new People();
+                pp.setAge(2 + r.nextInt());
+                queue.add(pp);
+            }
+
+        }else if(OFFER == monthName){
+            if(queue instanceof LinkedBlockingDeque){
+                if("1".equals(firstOrLast)){
+                    Random r = new Random();
+                    People pp = new People();
+                    pp.setAge(2 + r.nextInt());
+                    LinkedBlockingDeque queueNew = (LinkedBlockingDeque) queue;
+                    if(queueNew.offerFirst(pp)){
+                        System.out.println(queueNew.offerFirst(pp));
+                    }
+                    System.out.println(queueNew.offerFirst(pp));
+                }else{
+                    Random r = new Random();
+                    People pp = new People();
+                    pp.setAge(2 + r.nextInt());
+                    LinkedBlockingDeque queueNew = (LinkedBlockingDeque) queue;
+                    queueNew.offerLast(pp);
+                }
+            }else{
+                Random r = new Random();
+                People pp = new People();
+                pp.setAge(2 + r.nextInt());
+                queue.offer(pp);
+            }
+
+        }else if(PUT == monthName){
+            if(queue instanceof LinkedBlockingDeque){
+                if("1".equals(firstOrLast)){
+                    Random r = new Random();
+                    People pp = new People();
+                    pp.setAge(2 + r.nextInt());
+                    LinkedBlockingDeque queueNew = (LinkedBlockingDeque) queue;
+                    try {
+                        queueNew.putFirst(pp);
+                    } catch (InterruptedException e) {
+                        System.out.println("当前线程 "+ Thread.currentThread().getName() + "操作的putFirst方法出现异常");
+                    }
+                }else{
+                    Random r = new Random();
+                    People pp = new People();
+                    pp.setAge(2 + r.nextInt());
+                    LinkedBlockingDeque queueNew = (LinkedBlockingDeque) queue;
+                    try {
+                        queueNew.putLast(pp);
+                    } catch (InterruptedException e) {
+                        System.out.println("当前线程 "+ Thread.currentThread().getName() + "操作的putLast方法出现异常");
+                    }
+                }
+            }else {
+                Random r = new Random();
+                People pp = new People();
+                pp.setAge(2 + r.nextInt());
+                try {
+                    queue.put(pp);
+                } catch (InterruptedException e) {
+                    System.out.println("当前线程 " + Thread.currentThread().getName() + "操作的put方法出现异常");
+                }
+            }
+         }else{
+            System.out.println("出现错误！！！！！！！！！！！！");
+        }
+
     }
 }
---
+++ src/main/java/com/zhangjc/javaBasic/queue/blockingQueue/UseLinkedBlockingQueue.java
@@ -0,0 +1,29 @@
+package com.zhangjc.javaBasic.queue.blockingQueue;
+
+import com.zhangjc.javaBasic.queue.entry.People;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by user on 2018/9/19.
+ */
+public class UseLinkedBlockingQueue {
+
+    public static void main(String[] args) {
+        LinkedBlockingQueue<People> linkedBlockingDeque = new LinkedBlockingQueue();
+        String monthName1 = "offer";
+        String monthName2 = "poll";
+        ProductThread productThread = new ProductThread(linkedBlockingDeque,monthName1);
+        ConsumerThread consumerThread = new ConsumerThread(linkedBlockingDeque,monthName2);
+
+        for(int i = 0; i < 13; i++){
+            new Thread(productThread).start();
+        }
+
+        for(int i = 0; i < 12; i++){
+            new Thread(consumerThread).start();
+        }
+
+    }
+}
